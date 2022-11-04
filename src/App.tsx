d1c52ca5191bd1421aae468520f746af491a8bb1
Marcelearn <jaworskimarcel@gmail.com>
2022-11-04 09:44:56 +0100

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-
+import {useState, useEffect} from 'react'
 import './App.scss'
+import axios from 'axios';
+const nounsUrl = 'https://edwardtanguay.vercel.app/share/germanNouns.json';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nouns, setNouns] = useState([]);
+  useEffect(() => {
+    const response = axios.get(nounsUrl)
+
+  },[]);
 
   return (
     <div className="App">
       <h1>German Noun Site </h1>
+      <p>There are {nouns.length} npuns.</p>
+
 
       
       
